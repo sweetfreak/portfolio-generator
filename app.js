7f15a8f9151c252bc5d1b44ac5021e222e7dfc86
jesse sheehan <jessetsheehan@gmail.com>
2022-06-09 09:05:53 -0400

    added modules and created a webpage with node

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+/*
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-
 
 const printProfileData = profileDataArr => {
     //This...
@@ -7,7 +7,6 @@ const printProfileData = profileDataArr => {
       console.log(profileDataArr[i]);
     }
  
-
     console.log("===========");
 
 //Is the same as this...
@@ -16,3 +15,21 @@ const printProfileData = profileDataArr => {
  };
 
   printProfileData(profileDataArgs);
+*/
+//accesses the file system, or fs module.
+const fs = require("fs");
+
+//grabs the page-template function - generate page
+const generatePage = require("./src/page-template.js");
+
+//takes what's in the console and puts it into two array objects
+const profileDataArgs = process.argv.slice(2);
+//assigns the above into the array below
+const [name, github] = profileDataArgs;
+
+//creates a file using the generatePage function
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
